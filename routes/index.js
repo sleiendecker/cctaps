e88db2b33f3b5436c854494194e811c6c98ac803
Jamison Hyman <jhyman2@umbc.edu>
2015-09-27 17:47:32 -0400

    GET request working for beerslist

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/helloworld', function (req, res) {
+  res.render('helloworld', { title: 'Hello Baltimore Resident'});
+});
+
+router.get('/beerlist', function (req, res) {
+  var db = req.db;
+  var collection = db.get('beers');
+  collection.find({}, {}, function (error, docs) {
+    res.render('beerlist', {
+      'beerlist': docs
+    })
+  })
 });
 
 module.exports = router;
