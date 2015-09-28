9eb37747f0dd6341c0be094d0013d75fc5a7daf9
Jamison Hyman <jhyman2@umbc.edu>
2015-09-28 14:06:24 -0400

    spacing

--- scripts/max_scrape.js
+++ scripts/max_scrape.js
@@ -16,11 +16,11 @@ bars        = bar.bars;
 
 
 bars.forEach(function (bar) {
-  request(bm.url + bar.url, function(err, res, body) {
+  request(bm.url + bar.url, function (err, res, body) {
     $ = cheerio.load(body);
     console.log('\n\nOn tap at ' + bar.name);
     beers = $(bm.css);
-    $(beers).each(function(i, beer){
+    $(beers).each(function (i, beer) {
       console.log($(beer).text());
     });
   });
