47ae6ba7f4c0253f29ef93d014b67064eba8b55f
Jamison Hyman <jhyman2@umbc.edu>
2015-09-28 14:01:05 -0400

    Cleaning up max scrape

--- scripts/max_scrape.js
+++ scripts/max_scrape.js
@@ -4,39 +4,8 @@ bar 				= require('./sources.js'),
 bm 				  = bar.bm,
 bars        = bar.bars;
 
-
-
-
-// // maxs scrape
-// request(bar.maxs.url, function(err, resp, body){
-//   $ = cheerio.load(body);
-//   console.log('\n\nmaxs:')
-//   beers = $(bar.maxs.css);
-//   $(beers).each(function(i, beer){
-//     console.log($(beer).text());
-//   });
-// });
-
-
-// var get_tap_list = function(bar){
-// 	request(bm.url + bar.url, function(err, resp, body){
-// 	  $ = cheerio.load(body);
-// 	  console.log('\n\nOn tap at ' + bar.name);
-// 	  beers = $(bm.css);
-// 	  $(beers).each(function(i, beer){
-// 	    console.log($(beer).text());
-// 	  });
-// 	});
-// }
-
-var get_tap_list = function(arr, cb){
-  for(i = 0; i < bars.length; i ++){
-    cb(bars[i]);
-  }
-}
-
-get_tap_list(bars, function(bar){
-  request(bm.url + bar.url, function(err, resp, body){
+bars.forEach(function (bar) {
+  request(bm.url + bar.url, function(err, res, body) {
     $ = cheerio.load(body);
     console.log('\n\nOn tap at ' + bar.name);
     beers = $(bm.css);
@@ -45,5 +14,3 @@ get_tap_list(bars, function(bar){
     });
   });
 });
-
-
