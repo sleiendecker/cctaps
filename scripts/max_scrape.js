1c8c6877aa2ac1fda5e8180b5295bb87c0bfd43d
sleiendecker <sleiendecker@skylinenet.net>
2015-09-28 14:04:41 -0400

    re-added max scrape, handled more link

--- scripts/max_scrape.js
+++ scripts/max_scrape.js
@@ -4,6 +4,17 @@ bar 				= require('./sources.js'),
 bm 				  = bar.bm,
 bars        = bar.bars;
 
+// // maxs scrape
+// request(bar.maxs.url, function(err, resp, body){
+//   $ = cheerio.load(body);
+//   console.log('\n\nmaxs:')
+//   beers = $(bar.maxs.css);
+//   $(beers).each(function(i, beer){
+//     console.log($(beer).text());
+//   });
+// });
+
+
 bars.forEach(function (bar) {
   request(bm.url + bar.url, function(err, res, body) {
     $ = cheerio.load(body);
