316475d2f8c8a4eb16928aa8e6244160c0c4096c
Sam Leiendecker <samleiendecker@gmail.com>
2015-09-28 20:58:40 -0400

    added css attribute to bars' objects. refactored function to work with all bars

--- scripts/max_scrape.js
+++ scripts/max_scrape.js
@@ -1,27 +1,17 @@
 var cheerio = require('cheerio'),
 request		 	= require('request'),
 bar 				= require('./sources.js'),
-bm 				  = bar.bm,
 bars        = bar.bars;
 
-// // maxs scrape
-// request(bar.maxs.url, function(err, resp, body){
-//   $ = cheerio.load(body);
-//   console.log('\n\nmaxs:')
-//   beers = $(bar.maxs.css);
-//   $(beers).each(function(i, beer){
-//     console.log($(beer).text());
-//   });
-// });
-
-
 bars.forEach(function (bar) {
-  request(bm.url + bar.url, function (err, res, body) {
+  request(bar.url, function (err, res, body) {
     $ = cheerio.load(body);
-    console.log('\n\nOn tap at ' + bar.name);
-    beers = $(bm.css);
+    console.log('\n\n***On tap at ' + bar.name + '***\n');
+    beers = $(bar.css);
     $(beers).each(function (i, beer) {
       console.log($(beer).text());
     });
   });
 });
+
+
