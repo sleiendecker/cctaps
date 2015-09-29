d56aea6e8317c08f51d0ab37fb182c25ad0d1b13
Sam Leiendecker <samleiendecker@gmail.com>
2015-09-28 21:57:43 -0400

    renaming scrape file

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -7,7 +7,9 @@ bars.forEach(function (bar) {
   request(bar.url, function (err, res, body) {
     $ = cheerio.load(body);
     console.log('\n\n***On tap at ' + bar.name + '***\n');
+    $.load(bar.css);
     beers = $(bar.css);
+    console.log(beers.length);
     $(beers).each(function (i, beer) {
       console.log($(beer).text());
     });
@@ -15,3 +17,4 @@ bars.forEach(function (bar) {
 });
 
 
+ function foobar(el) { setTimeout(function() { foobar_cont(el); }, 5000); }
