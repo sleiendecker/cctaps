99f9a11063f6ac30b446957102d4c7b1c4630e8e
sleiendecker <sleiendecker@skylinenet.net>
2015-10-27 00:49:48 -0400

    added marnie

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,30 @@ var router = express.Router();
 var bar = require('../scripts/sources.js'),
 bars_arr = bar.bars;
 
+// /* GET home page. */
+// router.get('/', function (req, res, next) {
+//   // res.render('beerlist', { bar: bars });
+//   // res.render('bar', { test: "test" });
+//   // console.log(bars)
+//   var db = req.db;
+//   var collection = db.get('beers');
+//   collection.find({}, {}, function (error, docs) {
+//     res.render('beerlist', {
+//       'beerlist': docs
+//     })
+//   })
+// });
+
+
+
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  // res.render('beerlist', { bar: bars });
-  // res.render('bar', { test: "test" });
-  // console.log(bars)
-  var db = req.db;
-  var collection = db.get('beers');
-  collection.find({}, {}, function (error, docs) {
-    res.render('beerlist', {
-      'beerlist': docs
-    })
-  })
+  res.render('main', { title: bars_arr });
 });
+
+
+
 
 router.get('/helloworld', function (req, res) {
   res.render('helloworld', { title: 'Hello Baltimore Resident'});
