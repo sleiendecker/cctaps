2fc664339cbf15407a42848cf3e1b0ca1891995e
sleiendecker <sleiendecker@skylinenet.net>
2016-02-23 01:43:44 -0500

    bars now contain array of beer ids. still needs a ton of refactoring/cleaning up

--- scripts/untappd.js
+++ scripts/untappd.js
@@ -3,6 +3,8 @@
 // Imports
 var UntappdClient = require("../node_modules/node-untappd/UntappdClient",false);
 var MongoClient = require('mongodb').MongoClient;
+var MongoServer = "mongodb://localhost:27017/cctaps";
+var ObjectID = require('mongodb').ObjectID;
 var fs = require('fs');
 var util = require('util');
 var async = require('async');
@@ -24,30 +26,36 @@ var untappd = new UntappdClient(debug);
 untappd.setClientId(keys.clientId);
 untappd.setClientSecret(keys.clientSecret);
 
-var addToCollection = function(collection, object){
+var addToCollection = function(collection, object, cb){
   var mongoOpts = {
     set: {$set : object},
     upsert: { upsert: true}
   }
   if (object.hasOwnProperty('url')){
     collection.update( {url: object.url}, mongoOpts.set, mongoOpts.upsert)
-  } else {
-    collection.update( {lastUpdated: object.lastUpdated},mongoOpts.set, mongoOpts.upsert)
   }
+  cb(object._id);
+}
+
+
+
+var addBeerToBar = function(collection, id){
+    collection.update( {url: object.url}, mongoOpts.set, mongoOpts.upsert)
 }
 
 var connectToDb = function(err, beerObject, lastUpdated, cb){
   if (err) {
     console.log('Unable to add to collection');
   }else{
-    MongoClient.connect("mongodb://localhost:27017/cctaps", function(err, db) {
+    MongoClient.connect(MongoServer, function(err, db) {
       if(!err) {
-        console.log("Adding to ", beerObject.bar);
-        var collection = db.collection(beerObject.bar);
-        addToCollection(collection, beerObject);
-        addToCollection(collection, {lastUpdated: lastUpdated});
+        var collection = db.collection('beers');
+        addToCollection(collection, beerObject, function(id){
+          // Add id to bar collection
+          db.collection('bars').update({ name: beerObject.bar },{$push: {'beers': {_id: id}}});
+        });
         db.close();
-        }
+      }
     });
   }
   cb();
@@ -69,15 +77,36 @@ function getUrl(beerName, cb) {
 function buildObject(beer, bar, url, cb){
   var beer = beer[0];
   var dbBeer = {
-    'bar' : bar,
-    'brewery' : beer.brewery_name,
-    'name' : beer.beer_name,
-    'abv' : beer.beer_abv,
-    'rating': beer.ba_score,
-    'style' : beer.beer_style,
-    'url': url
+    _id: new ObjectID,
+    bar : bar,
+    brewery : beer.brewery_name,
+    name : beer.beer_name,
+    abv : beer.beer_abv,
+    rating: beer.ba_score,
+    style : beer.beer_style,
+    url: url
   }
   cb(null, dbBeer)
+}
+
+function checkIfBarExists(bar, db, cb){
+  console.log('Checking if ' + bar.name + ' exists');
+  db.collection('bars').findOne({'bar': bar.bar}, function(err, doc){
+    cb(doc);
+  });
+}
+
+
+function checkLastUpdated(bar, db, newBar, cb){
+  var upToDate;
+  db.collection('bars').findOne({'name': bar.name}, function(err, doc){
+    if (doc.lastUpdated < bar.lastUpdated || (newBar)) {
+      upToDate = false;
+    } else {
+      upToDate = true;
+    }
+    cb(upToDate);
+  });
 }
 
 /**
@@ -85,18 +114,49 @@ function buildObject(beer, bar, url, cb){
  * array of beer strings.
  */
 function getBeers(bar, cb){
-  console.log('getting beers for bar: ' + bar.name);
+  var newBar;
+  // console.log('getting beers for bar: ' + bar.name);
   request(bar.url, function (err, res, body) {
-    var $          = cheerio.load(body),
-    beers          = $(bar.css),
-    beersFormatted = [],
-    lastUpdated    = $('.text-oranges').text();
-    lastUpdated    = lastUpdated.substring(lastUpdated.indexOf(":") + 1);
-    // lastUpdated.date = theDate;
+    var $             = cheerio.load(body),
+    beers             = $(bar.css),
+    beersFormatted    = [],
+    rawUpdated        = $('.text-oranges').text(),
+    formattedUpdated  = rawUpdated.substring(rawUpdated.indexOf(":") + 1);
+    bar.lastUpdated   = formattedUpdated;
     $(beers).each(function(i, beer) {
       beersFormatted.push($(beer).text().trim());
     });
-    cb(null, beersFormatted, lastUpdated);
+
+    var dbBar = {
+      name: bar.name,
+      lastUpdated: bar.lastUpdated,
+      beers: []
+    }
+
+    MongoClient.connect(MongoServer, function(err, db) {
+      checkIfBarExists(dbBar, db, function(status) {
+        // If the bar doesn't exist in the db
+        if (status === null){
+          newBar = true;
+          var collection = db.collection("bars");
+          collection.insert(dbBar);
+        }
+        db.close();
+      });
+    });
+
+    MongoClient.connect(MongoServer, function(err, db) {
+      checkLastUpdated(bar, db, newBar, function(res) {
+        db.close();
+        if (res){
+          console.log('Up to date');
+        } else{
+          console.log('Updating')
+          cb(null, beersFormatted, bar.lastUpdated);
+        }
+      });
+    });
+
   });
 }
 
