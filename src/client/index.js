d0b9bec7ded0bf3c8f96f04f6bb07c7764cb458c
Jamison Hyman <jhyman2@umbc.edu>
2016-03-07 11:20:45 -0500

    debugging

--- src/client/index.js
+++ src/client/index.js
@@ -2,7 +2,5 @@ import React                         from 'react';
 import ReactDOM                      from 'react-dom';
 import BeerGrid                      from './beer_grid';
 
-window.onload = () => {
-  console.log('am i getting here index');
-  ReactDOM.render(<BeerGrid />, document.getElementById('app-container'));
-};
+console.log('am i getting here index');
+ReactDOM.render(<BeerGrid />, document.getElementById('app-container'));
